cc2caf8632628135d63e309f38187d6d74ab1e9e
Longtcs <longtcs@smartosc.com>
2020-06-05 18:36:52 +0700

    unit test following-manager success

--- src/test/java/com/backend/template/controller/FollowControllerTest.java
+++ src/test/java/com/backend/template/controller/FollowControllerTest.java
@@ -3,12 +3,16 @@ package com.backend.template.controller;
 import com.backend.template.exception.DuplicateException;
 import com.backend.template.service.FollowService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.Mockito.when;
 
@@ -32,12 +36,12 @@ public class FollowControllerTest  {
     @Test
     public void testFollowDuplicateId() throws Exception{
         DuplicateException duplicateException = new DuplicateException("Duplicate Id");
-        when(followService.followUser(userid , followid).thenReturn(duplicateException);
+        when(followService.followUser(Mockito.anyLong(), Mockito.anyLong() )).thenThrow(duplicateException);
         this.mockMvc.perform(
-                get("/follower/following?followid=1", followid)
-                        .header("Authorization", "Bearer " + this.accessToken)
+                MockMvcRequestBuilders.post("/follower/following?followid=1")
+                        .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsicmVzb3VyY2VfaWQiXSwidXNlcl9uYW1lIjoiMSIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSIsInRydXN0Il0sImV4cCI6MTU5MTM2MDEzNywiYXV0aG9yaXRpZXMiOlsiUk9MRV9BRE1JTiJdLCJqdGkiOiJjMWQwMzdjMi02YjYwLTRlOTEtYmQ5Mi01NDVkMGM0NTQ0MzEiLCJjbGllbnRfaWQiOiJkZXZnbGFuLWNsaWVudCJ9.nvurM9uDkrB3e9SDzmA9_TBu9Q6BGksTejHVv4ktqVs")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
 
     }
 }
